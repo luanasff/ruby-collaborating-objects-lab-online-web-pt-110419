d85c990f0b60a913a056c825a506300175c673e9
Luana Faria Lane <luanasfff@gmail.com>
2020-01-05 00:59:59 +0000

    Done.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+
+
+
 # Ruby Collaborating Objects Lab
 
 ## Objective
---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song 
+  attr_accessor :artist, :name   
+  @@all = []
+
+  def initialize(name)
+    @name = name 
+    save 
+  end 
+
+  def save 
+    @@all << self 
+  end 
+
+
+  def self.all
+    #Artist.all 
+    @@all 
+  end 
+
+  def self.new_by_filename(file_name)
+    artist = file_name.split(" - ")[0]
+    song_title = file_name.split(" - ")[1]
+    song = self.new(song_title)
+    song.artist_name = artist
+    song 
+  end 
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self) 
+  end 
+end
